31f31a7efcb9084a55247d771f3dc9a217de94c8
Camilo Hernandez <camilo.hernandez.castillo@gmail.com>
2024-09-03 12:05:03 -0500

    FIT-123: addapting physical test for omron measures (#205)
    Co-authored-by: Camilo Hernandez <camehernandecas@unal.edu.co>

--- resources/views/cliente/physicalTest.blade.php
+++ resources/views/cliente/physicalTest.blade.php
@@ -3,14 +3,15 @@
     <form id="physicalForm">
         @csrf
         <x-input name="weight" description="Peso (Kg)" type="number" required step=".01"></x-input>
+        <x-input name="body_fat" description="Grasa Corporal" type="number" required step=".01"></x-input>
         <x-input name="muscle" description="Músculo" type="number" required step=".01"></x-input>
         <x-input name="visceral_fat" description="Grasa Visceral" type="number" required step=".01"></x-input>
-        <x-input name="body_fat" description="Grasa Corporal" type="number" required step=".01"></x-input>
-        <x-input name="water_level" description="Nivel agua:" type="number" required step=".01"></x-input>
-        <x-input name="proteins" description="Proteínas:" type="number" required step=".01"></x-input>
+
+        <!--<x-input name="water_level" description="Nivel agua:" type="number" required step=".01"></x-input>-->
+        <!--<x-input name="proteins" description="Proteínas:" type="number" required step=".01"></x-input>-->
         <x-input name="basal_metabolism" description="Metabolismo Basal:" type="number" required step=".01"></x-input>
-        <x-input name="bone_mass" description="Masa Ósea:" type="number" required step=".01"></x-input>
-        <x-input name="body_score" description="Puntuación Corporal:" type="number" required step=".01"></x-input>
+        <!--<x-input name="bone_mass" description="Masa Ósea:" type="number" required step=".01"></x-input>-->
+        <!--<x-input name="body_score" description="Puntuación Corporal:" type="number" required step=".01"></x-input>-->
         <div class="d-flex justify-content-between">
             <p class="btn btn-danger mt-3 mx-auto" onclick="hidePhysicalSection()">Quitar Sección física</p>
             <button class="btn themed-btn mt-3 mx-auto d-block" type="submit">Guardar Sección</button>
@@ -46,15 +47,15 @@
                 method: "POST",
                 data: {
                     user_id : {{$user->id}},
-                    muscle : document.getElementById('muscle').value,
-                    visceral_fat: document.getElementById('visceral_fat').value,
-                    body_fat: document.getElementById('body_fat').value,
-                    water_level: document.getElementById('water_level').value,
-                    proteins: document.getElementById('proteins').value,
-                    basal_metabolism: document.getElementById('basal_metabolism').value,
-                    bone_mass: document.getElementById('bone_mass').value,
-                    body_score: document.getElementById('body_score').value,
-                    weight : document.getElementById('weight').value,
+                    muscle : document.getElementById('muscle')?.value,
+                    visceral_fat: document.getElementById('visceral_fat')?.value,
+                    body_fat: document.getElementById('body_fat')?.value,
+                    water_level: document.getElementById('water_level')?.value,
+                    proteins: document.getElementById('proteins')?.value,
+                    basal_metabolism: document.getElementById('basal_metabolism')?.value,
+                    bone_mass: document.getElementById('bone_mass')?.value,
+                    body_score: document.getElementById('body_score')?.value,
+                    weight : document.getElementById('weight')?.value,
                 },
                 success: handleAjaxResponse,
                 error: handleAjaxResponse
