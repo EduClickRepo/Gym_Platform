404c989afab64cfb4df9d911ed5ba75cdc6e758a
Camilo Hernandez <camilo.hernandez.castillo@gmail.com>
2025-02-08 09:11:38 -0500

    checkbox for plan contract

--- resources/views/planCard.blade.php
+++ resources/views/planCard.blade.php
@@ -160,6 +160,78 @@
             </tbody>
         </table>
     </div>
+    <form id="paymentForm{{$plan->id}}">
+        @auth()
+            <div class="form-check m-auto">
+                <input class="form-check-input" type="checkbox" name="aceptacion" id="aceptacion" required>
+                <label class="form-check-label terms-label" for="aceptacion">
+                    <small>Acepto el
+                        <a style="text-decoration: none" href="javascript:void(0);" data-toggle="modal" data-target="#modalTerminos">
+                            <b><u>Contrato de Servicio</u></b>
+                        </a>
+                    </small>
+                </label>
+            </div>
+        @endauth
 
-    <a style="bottom: -20px" class="btn color-white themed-btn mt-3 position-absolute" @if(auth()->guest()) href="{{ route('login') }}" @else onclick="showPayModal({{ $plan }}, this.closest('.card').querySelector('.payment-options'))" @endif>Seleccionar</a>
+        <button type="submit" class="btn color-white themed-btn mt-3 position-absolute">
+            Seleccionar
+        </button>
+    </form>
 </div>
+
+@push('scripts')
+    <script>
+        document.getElementById("paymentForm{{$plan->id}}").addEventListener("submit", submitListener, true);
+
+        function submitListener(event) {
+            event.preventDefault();
+            // Si el usuario no está autenticado, redirigir al login
+            @auth()
+                showPayModal(@json($plan), document.querySelector('.payment-options'));
+            @else
+                window.location.href = "{{ route('login') }}";
+                return false; // Evita que el formulario se envíe automáticamente
+            @endauth
+        }
+    </script>
+@endpush
+
+@push('modals')
+    <!-- Modal terminos y condiciones-->
+    <div class="modal m-auto" tabindex="-1" role="dialog" id="modalTerminos" style="height: 100vh; width: 75vw; z-index: 1051">
+        <div role="document" class="m-auto h-100 w-100">
+            <div class="modal-content h-100">
+                <div class="modal-header h-100">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    <object data="{{asset('pdf/Terminos_y_condiciones_V2-GRL_PWR.pdf')}}" type="application/pdf" frameborder="0" width="100%" height="100%" style="padding: 20px;">
+                        <embed src="{{asset('pdf/Terminos_y_condiciones_V2-GRL_PWR.pdf')}}" type='application/pdf' width="100%" height="100%" />
+                    </object>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal m-auto" tabindex="-1" role="dialog" id="modalConsentimiento" style="z-index: 1051!important; height: 100vh; width: 75vw;">
+        <div role="document" class="m-auto h-100 w-100">
+            <div class="modal-content h-100">
+                <div class="modal-header h-100">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    <object data="{{asset('pdf/Consentimiento_InformadoV2-GRL_PWR.pdf')}}" type="application/pdf" frameborder="0" width="100%" height="100%" style="padding: 20px;">
+                        <embed src="{{asset('pdf/Consentimiento_InformadoV2-GRL_PWR.pdf')}}" type='application/pdf' width="100%" height="100%" />
+                    </object>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+@endpush
